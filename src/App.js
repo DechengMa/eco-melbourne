9d5f0a33c32b1ae048dcc663eaadbe12a4ca9020
Decheng <chinadecheng@gmail.com>
2019-05-02 14:46:02 +1000

    password stuff

--- src/App.js
+++ src/App.js
@@ -71,9 +71,9 @@ const App = () => {
 
 	// Password
 	const inputPassword = event => {
-		if (process.env.NODE_ENV === 'development') {
-			setShowContent(true);
-		}
+		// if (process.env.NODE_ENV === 'development') {
+		// 	setShowContent(true);
+		// }
 
 		if (event.target.value === process.env.REACT_APP_PASSWORD) {
 			setShowContent(true);
