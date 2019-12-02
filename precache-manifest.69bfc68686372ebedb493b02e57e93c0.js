ae08c83d932dc8ecf5c201b2dffd5cf6471d904c
Decheng <chinadecheng@gmail.com>
2019-12-02 22:16:47 +1100

    Updates

---
+++ precache-manifest.69bfc68686372ebedb493b02e57e93c0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a6f4286932c176c7754",
+    "url": "/DechengMa/eco-mel/static/css/main.51b4bd68.chunk.css"
+  },
+  {
+    "revision": "7a6f4286932c176c7754",
+    "url": "/DechengMa/eco-mel/static/js/main.f3e8597c.chunk.js"
+  },
+  {
+    "revision": "857a39ae39d2191beec5",
+    "url": "/DechengMa/eco-mel/static/js/runtime~main.2742affc.js"
+  },
+  {
+    "revision": "3da7a15d358fa723d8cf",
+    "url": "/DechengMa/eco-mel/static/css/2.a4032578.chunk.css"
+  },
+  {
+    "revision": "3da7a15d358fa723d8cf",
+    "url": "/DechengMa/eco-mel/static/js/2.e5194eee.chunk.js"
+  },
+  {
+    "revision": "f03b4067edd4856181e8227ba1e26cb1",
+    "url": "/DechengMa/eco-mel/static/media/bicycle.f03b4067.png"
+  },
+  {
+    "revision": "bb43ba8a637cc3bbf52ab7435fa90926",
+    "url": "/DechengMa/eco-mel/static/media/environmentalIssue.bb43ba8a.png"
+  },
+  {
+    "revision": "b890eba98176681b709a538cf640ce2f",
+    "url": "/DechengMa/eco-mel/static/media/healthyIssue.b890eba9.png"
+  },
+  {
+    "revision": "6964489333a6df9ac2800a66f1eb2803",
+    "url": "/DechengMa/eco-mel/static/media/Angie.69644893.png"
+  },
+  {
+    "revision": "fe925b2fc492ab8f020fc1cdb8af39b2",
+    "url": "/DechengMa/eco-mel/static/media/Matt.fe925b2f.png"
+  },
+  {
+    "revision": "40286110d7af13ff4df5244320ea48bc",
+    "url": "/DechengMa/eco-mel/static/media/Nitya.40286110.png"
+  },
+  {
+    "revision": "aad6c1e61db139b3a84705894c6e77b7",
+    "url": "/DechengMa/eco-mel/static/media/Vig.aad6c1e6.png"
+  },
+  {
+    "revision": "f0e8ca4e1f060f680cd3d1fc7c3b1d35",
+    "url": "/DechengMa/eco-mel/static/media/Gavin.f0e8ca4e.png"
+  },
+  {
+    "revision": "13e176f8cc52e05d03ab8c7af5a4c566",
+    "url": "/DechengMa/eco-mel/static/media/404NotFound.13e176f8.png"
+  },
+  {
+    "revision": "8b910ea1df0ea16a0d2d08f6b8877533",
+    "url": "/DechengMa/eco-mel/index.html"
+  }
+];
